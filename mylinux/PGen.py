284d735cf03650501b5692fddc6ca02d9c0d7385
renwl <wenliang.ren@quanray.com>
2016-10-25 19:15:38 +0800

    为了更改参数方便

--- mylinux/PGen.py
+++ mylinux/PGen.py
@@ -4,35 +4,52 @@ sys.path.append("./mypy")
 import os
 import re
 from NetChange import *
-ListStart=1e-3
-ListStep=1e-3
-ListStop=1e-3
-temp_parlist=[-40,27,85]
-cor_parlist=["snfp","ss","tt","ff"]
-vdd_parlist=[1.4,1.5]
+SimCond={}
+SimCond["temp"]=[-40,27,85]
+SimCond["cor"]=["snfp","ss","tt","ff"]
+SimCond["vdd"]=[1.4,1.5]
+SimCond["gain"]=[i*1e-3 for i in range(1,11,1)]
+SimCond["rl"]=[2.1e3,3.1e3]
+
+
 
 if len(sys.argv) < 2:
 	print "please input simulation filename!"
 else:
 	filename=sys.argv[1].split(r".")[0]
-	print "filename=",filename
-	x=ListStart
-	gain_parlist=[x];
-	while 1:
-		x+=ListStep
-		if x >  ListStop:
-			break
-		gain_parlist.append(x)
+	#print "filename=",filename
 	#SimChoice=int(input("please input 1 for new simulation,0 for continue"))
 	SimChoice=1
 	if SimChoice==1 :
 		index=0
 		f=open(filename + r"_CorList.txt","w")
-		for gain_par in gain_parlist:
-			for vdd_par in vdd_parlist:
+		keylist=[]
+		lenpar=1
+		for keys in SimCond:
+			keylist.append(keys)
+			lenpar*=len(SimCond[keys])
+		#print keylist
+		#print "length=",lenpar
+		ParTemp={}
+		for paridx in range(0,lenpar,1):
+			strtemp="index," + str(paridx)
+			ParTemp[paridx]=[strtemp]
+		for keys in SimCond:
+			lentemp=len(SimCond[keys])
+			for i in ParTemp:
+				strtemp=keys + "," + str(SimCond[keys][i%lentemp])
+				ParTemp[i].append(strtemp)	
+		for keys in ParTemp:
+			strtemp=",".join(ParTemp[keys]) + ";\n"
+			#print keys,"------",strtemp
+			f.write(strtemp)
+		f.close()
+"""
+			for par in vdd_parlist:
 				for temp_par in temp_parlist:
 					for cor_par in cor_parlist:
 						f.write("index," + str(index) + "," + "gain," + str(gain_par) + "," + "vdd," + str(vdd_par) + "," + 
 						"temp," + str(temp_par) + "," + "cor," + str(cor_par) + ";\n")
 						index+=1
 		f.close()
+"""
