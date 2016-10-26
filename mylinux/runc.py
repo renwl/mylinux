1a82f18f71e00fe7da44cf9b68f80b74288c0e07
renwl <wenliang.ren@quanray.com>
2016-10-26 18:58:15 +0800

    使得运行更方便

--- mylinux/runc.py
+++ mylinux/runc.py
@@ -4,38 +4,24 @@ sys.path.append("./mypy")
 import os
 import re
 from NetChange import *
-ListStart=1e-3
-ListStep=1e-3
-ListStop=100e-3
-temp_parlist=[-40,27,85]
-cor_parlist=["snfp","fnsp","ss","tt","ff"]
-vdd_parlist=[1.35,1.55]
+from FileSize import *
 
 if len(sys.argv) < 2:
 	print "please input simulation filename!"
 else:
-	temp1=re.split(r'/',sys.argv[1])
-	temp=re.split('\.',temp1[-1])
-	x=ListStart
-	gain_parlist=[x];
-	while 1:
-		x+=ListStep
-		if x >  ListStop:
-			break
-		gain_parlist.append(x)
+	SimPath=re.split(r'/',sys.argv[1])
+	SimFile=re.split('\.',SimPath[-1])
 	SimChoice=int(input("please input 1 for new simulation,0 for continue"))
 	if SimChoice==1 :
-		f=open(temp[0] + "_CorList.txt","w")
-		for gain_par in gain_parlist:
-			for vdd_par in vdd_parlist:
-				for temp_par in temp_parlist:
-					for cor_par in cor_parlist:
-						f.write(str(gain_par) + " " + str(vdd_par) + " " + str(temp_par) + " " + str(cor_par) + "\n")
+		StartIndex = 0
+	else:
+		f=open(SimFile[0] + r"_IndexList.txt")
+		f_data=f.readline()
+		StartIndex = int(f_data.split("=")[-1])
 		f.close()
-
-
+	print "StartIndex=",StartIndex
 	if SimChoice==1 :
-		f=open(temp[0] + r".csv","w")
+		f=open(SimFile[0] + r".csv","w")
 		f.write("start simulation\n")
 		f.close()
 	f_sim=open(sys.argv[1])
@@ -57,27 +43,42 @@ else:
 		f_sim.write(i)
 	f_sim.close()
 
-	f=open(temp[0] + "_CorList.txt","r")
-	PatternData=f.readlines()
-	StoreData=PatternData[:]
+	PatternData=[]
+	f=open(SimFile[0] + r"_CorList.txt","r")
+	for i in f:
+		temp1=i.split(";")[0]
+		temp2=temp1.split(",")
+		#print temp2
+		TempData={}
+		for k in range(0,len(temp2)-1,2):
+			TempData[temp2[k]]=temp2[k+1]
+			#print TempData
+		PatternData.append(TempData)
+		#PatternData=f.readlines()
 	f.close()
-	
 	for patternline in PatternData:
+		if int(patternline["index"]) < StartIndex:
+			continue
+		#print "index=",patternline["index"]
 		f_sim=open(sys.argv[1])
-		l=patternline.split(" ")
 		LData=[]
 		tt=""
 		for eachline in f_sim:
-			if re.search("gain=",eachline):
-				tt=ParCh("gain",l[0],eachline)
-				tt=ParCh("vdd",l[1],tt)
+			if re.search("^parameters",eachline):
+				for parkey in patternline:
+					if parkey == "index" or parkey == "temp" or parkey == "cor":
+						continue
+					else:
+						tt=ParCh(parkey,patternline[parkey],eachline)
+						eachline=tt
+						#print "tt=",tt
 				LData.append(tt)
 			elif re.search("temp=",eachline):
-				tt=ParCh("temp",l[2],eachline)
+				tt=ParCh("temp",patternline["temp"],eachline)
 				LData.append(tt)
 			elif re.search("section=",eachline):
-				tt=CorCh(l[3],eachline)
-				LData+=tt
+				tt=CorCh(patternline["cor"],eachline)
+				LData+=tt 
 			else:
 				LData.append(eachline)
 		f_sim.close()
@@ -85,10 +86,11 @@ else:
 		for i in LData:
 			f1.write(i)
 		f1.close()
-		LocalPath=r'spectremdl ++aps +mt=4 -format psfbin -batch ' + temp[0] + '_runc.mdl -tab -design ./' + temp1[-1]
-		LocalPath=LocalPath + r" -measure " + temp[0] + r'.measure'
+
+		LocalPath=r'spectremdl ++aps +mt=4 -format psfbin -batch ' + SimFile[0] + '_runc.mdl -tab -design ./' + SimPath[-1]
+		LocalPath=LocalPath + r" -measure " + SimFile[0] + r'.measure'
 		os.system(LocalPath)
-		
+
 		f=open(temp[0] + r".csv","a")
 		f.write("gain=" + l[0] + " vdd=" + l[1] + " temp=" + l[2] + " corner=" + l[3])
 		f1=open(temp[0] + r'.measure')
@@ -103,9 +105,13 @@ else:
 		f.close()
 		f1.close()
 	
-		StoreData.pop(0)
-		f=open(temp[0] + "_CorList.txt","w")
-		for i in StoreData:
-			f.write(i)
+		StartIndex+=1
+		f=open(SimFile[0] + r"_IndexList.txt","w")
+		f.write("index=" + str(StartIndex))
 		f.close()
-
+		f_size=filesize(".")
+		if f_size > 50e6:
+			print "overflow",f_size
+			input("continue?")
+		else:
+			print "safe",f_size
